115e33cf8638fd73554466ec8a30d16d0920f7f7
yarentamir <yarden.tamir@walla.co.il>
2021-12-02 17:29:16 +0200

    start

---
+++ js/Mincraft.js
@@ -0,0 +1,66 @@
+"use strict";
+const gameBoard = document.querySelector("#gameBoard");
+
+const gameBoardMatrix = [
+  //18*18
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 6, 6, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 6, 6, 6, 6, 6, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 6, 6, 6, 6, 6, 0, 0, 0, 0, 2, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 2, 2, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 2, 2, 2, 2, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 2, 2, 2, 2, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 2, 2, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0],
+  [0, 0, 2, 2, 0, 0, 3, 3, 3, 0, 0, 0, 0, 1, 0, 0, 0, 0],
+  [0, 0, 2, 2, 0, 0, 3, 3, 3, 0, 0, 0, 0, 1, 0, 0, 0, 0],
+  [4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4],
+  [4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4],
+  [4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4],
+  [4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4],
+];
+
+const materialObj = {
+  tree: "tree",
+  leaves: "leaves",
+  rock: "rock",
+  ground: "ground",
+  grass: "grass",
+  cloud: "cloud",
+};
+
+// runs on each row
+gameBoardMatrix.forEach((row, yIndex) => {
+  // runs on each column
+  row.forEach((column, xIndex) => {
+    // save current position id
+    const currentPositionId = column;
+    // create a block
+    const block = document.createElement("div");
+    // add style by id
+    switch (currentPositionId) {
+      case 1:
+        block.classList.add(materialObj.tree);
+        break;
+      case 2:
+        block.classList.add(materialObj.leaves);
+        break;
+      case 3:
+        block.classList.add(materialObj.rock);
+        break;
+      case 4:
+        block.classList.add(materialObj.ground);
+        break;
+      case 5:
+        block.classList.add(materialObj.grass);
+        break;
+      case 6:
+        block.classList.add(materialObj.cloud);
+        break;
+    }
+    gameBoard.appendChild(block);
+  });
+});
