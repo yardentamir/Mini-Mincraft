945d11d81cbe0978f197bb000d35b3be699bb565
yarentamir <yarden.tamir@walla.co.il>
2021-12-03 19:42:30 +0200

    material bar done

--- js/Mincraft.js
+++ js/Mincraft.js
@@ -1,6 +1,6 @@
 "use strict";
 const ARRAY_OF_TOOLS = ["axe", "pickaxe", "shovel"];
-const MATERIAL_ARRAY = [
+const ARRAY_MATERIAL = [
   ["sky", -1],
   ["tree", 0],
   ["leaves", 0],
@@ -10,9 +10,16 @@ const MATERIAL_ARRAY = [
   ["cloud", -1],
 ];
 
+const arrOfMaterials = ARRAY_MATERIAL.flat(Infinity).filter(
+  (material) => typeof material === "string"
+);
+
 const gameBoardElement = document.querySelector("#gameBoard");
 const toolBoxElement = document.querySelector(".toolBox");
 const materialBoxElement = document.querySelector(".materialBar");
+const lastClickedMaterialElement = document.querySelector(
+  "#lastClickedMaterial"
+);
 
 const mainInfo = {
   isStartGame: false,
@@ -34,10 +41,11 @@ const mainInfo = {
     [0, 0, 2, 2, 0, 0, 3, 3, 3, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0],
     [5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5],
     [4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4],
-    [4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4],
-    [4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4],
+    [4, 4, 4, 3, 3, 4, 4, 4, 3, 4, 4, 4, 4, 3, 4, 4, 4, 4, 4],
+    [4, 4, 3, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 3, 4, 4],
   ],
   selectedTool: -1, // axe = 0 // pickaxe =1 // shovel = 2
+  selectedMaterial: -1, // 1 = wood // 2 = levees // 3 = metal // 4 = dirt // 6 = clouds
   lastClickedMaterial: -1, // 1 = wood // 2 = levees // 3 = metal // 4 = dirt // 6 = clouds
 };
 
@@ -46,15 +54,12 @@ const gameInit = () => {
   ARRAY_OF_TOOLS.forEach((tool) => {
     createToolsBar(tool);
   });
-  const filteredMaterials = MATERIAL_ARRAY.filter(
+  const filteredMaterials = ARRAY_MATERIAL.filter(
     (material) => material[1] !== -1
   );
-  filteredMaterials.forEach((material, materialIndex) => {
-    createMaterialsBar(material[0], materialIndex);
+  filteredMaterials.forEach((material) => {
+    createMaterialsBar(material[0], ARRAY_MATERIAL.indexOf(material));
   });
-  // document.querySelectorAll(".swords").forEach((tool) => {
-  //   tool.addEventListener("click", whichToolSelected);
-  // });
 };
 
 const createToolsBar = (tool) => {
@@ -63,10 +68,7 @@ const createToolsBar = (tool) => {
   createButtonTool.classList.add(tool);
   toolBoxElement.appendChild(createButtonTool);
   createButtonTool.dataset.toolId = ARRAY_OF_TOOLS.indexOf(tool);
-  createButtonTool.addEventListener("click", whichToolSelected);
-
-  // const createButtonTool = `<button class="swords ${tool}"></button>`;
-  // toolBoxElement.innerHTML += createButtonTool;
+  createButtonTool.addEventListener("click", ToolOrMaterialSelected);
 };
 
 const createMaterialsBar = (materialName, materialIndex) => {
@@ -78,7 +80,7 @@ const createMaterialsBar = (materialName, materialIndex) => {
   createButtonMaterial.appendChild(createSpanButtonMaterial);
   materialBoxElement.appendChild(createButtonMaterial);
   createButtonMaterial.dataset.materialBoxId = materialIndex;
-  createButtonMaterial.addEventListener("click", whichToolSelected);
+  createButtonMaterial.addEventListener("click", ToolOrMaterialSelected);
 };
 
 const printGameBoard = () => {
@@ -90,42 +92,118 @@ const printGameBoard = () => {
       const block = document.createElement("div");
 
       block.dataset.materialId = currentPositionId;
-      block.addEventListener("click", clickHandler);
-      // block.addEventListener("mouseover", mouseOverHandler);
-      // block.addEventListener("mouseout", mouseOutHandler);
-      block.classList.add(MATERIAL_ARRAY[currentPositionId][0]);
+      block.addEventListener("click", gameBoardClickHandler);
+      block.classList.add(ARRAY_MATERIAL[currentPositionId][0]);
       gameBoardElement.appendChild(block);
     });
   });
 };
 
-const whichToolSelected = (e) => {
-  const selectedToolElement = e.target;
+const ToolOrMaterialSelected = ({ target }) => {
+  if (target.dataset.toolId) {
+    userSelectedTool(target);
+    return;
+  }
+  userSelectedMaterial(target);
+  if (mainInfo.selectedTool > -1)
+    document.querySelector(".selected-tool").classList.remove("selected-tool");
+};
+
+const userSelectedTool = (selectedToolElement) => {
   const toolId = selectedToolElement.dataset.toolId;
   mainInfo.selectedTool = toolId;
+  removeMaterialSelection();
+  removeToolSelection();
+  selectedToolElement.classList.add("selected-tool");
+};
+
+const userSelectedMaterial = (target) => {
+  mainInfo.selectedTool = -1;
+  removeToolSelection();
+  if (target.firstChild.textContent > 0) {
+    removeMaterialSelection();
+    target.firstChild.classList.add("selected-material");
+    mainInfo.selectedMaterial = target.dataset.materialBoxId;
+    return;
+  }
+  console.log(target.firstChild.textContent);
+};
+
+const removeMaterialSelection = () => {
+  if (document.querySelector(".selected-material")) {
+    document
+      .querySelector(".selected-material")
+      .classList.remove("selected-material");
+  }
+  mainInfo.selectedMaterial = -1;
+};
+
+const removeToolSelection = () => {
   const allTools = document.querySelectorAll("[data-tool-id]");
   allTools.forEach((eachTool) => {
     eachTool.classList.remove("selected-tool");
   });
-  selectedToolElement.classList.add("selected-tool");
 };
 
-const clickHandler = (e) => {
-  const item = e.target;
-  const saveMaterialId = item.dataset.materialId;
+const gameBoardClickHandler = ({ target }) => {
+  const saveMaterialId = target.dataset.materialId;
   const selectedTool = +mainInfo.selectedTool;
-  if (MATERIAL_ARRAY[saveMaterialId][1] === selectedTool) {
-    item.classList.remove(MATERIAL_ARRAY[saveMaterialId][0]);
+  //put last material in sky
+  putLastMaterialInSky(target);
+  // not
+  putSelectedMaterialInSky(target, saveMaterialId);
+  if (selectedTool === -1) return;
+  // console.log(arrOfMaterials);
+  //for any click on game board
+  const lastClickedMaterialClasses = [...lastClickedMaterialElement.classList];
+  lastClickedMaterialClasses.forEach((classs) => {
+    if (arrOfMaterials.includes(classs)) {
+      lastClickedMaterialElement.classList.remove(classs);
+    }
+  });
+  mainInfo.lastClickedMaterial = target.dataset.materialId;
+  const lastClickedMaterial = mainInfo.lastClickedMaterial;
+  // match between tool and material
+  if (saveMaterialId && ARRAY_MATERIAL[saveMaterialId][1] === selectedTool) {
+    target.classList.remove(ARRAY_MATERIAL[saveMaterialId][0]);
+    lastClickedMaterialElement.classList.add(
+      ARRAY_MATERIAL[lastClickedMaterial][0]
+    );
+    target.dataset.materialId = 0;
+    addToInventory(lastClickedMaterial);
+    // not a match
   } else {
     const selectedToolElement = document.querySelector(
       `[data-tool-id='${selectedTool}']`
     );
-    selectedToolElement.classList.toggle("invalid");
+    selectedToolElement.classList.add("invalid");
     setTimeout(() => {
-      selectedToolElement.classList.toggle("invalid");
+      selectedToolElement.classList.remove("invalid");
     }, 500);
   }
-  // if(myObj.hasOwnProperty('key')   classlistMaterial.contain)
+};
+
+const putLastMaterialInSky = (target) => {};
+
+const putSelectedMaterialInSky = (target, selectedBlockId) => {
+  const whichMaterialNum = mainInfo.selectedMaterial;
+  const materialElement = document.querySelector(".selected-material");
+  if (materialElement) {
+    if (selectedBlockId != 0 || materialElement.textContent < 1) return;
+    materialElement.textContent--;
+    if (materialElement.textContent == 0)
+      materialElement.classList.remove("selected-material");
+    target.classList.remove("sky");
+    target.dataset.materialId = whichMaterialNum;
+    target.classList.add(ARRAY_MATERIAL[whichMaterialNum][0]);
+  }
+};
+
+const addToInventory = (lastClickedMaterial) => {
+  const MaterialInventoryElement = document.querySelector(
+    `[data-material-box-id='${lastClickedMaterial}']`
+  );
+  MaterialInventoryElement.firstChild.textContent++;
 };
 
 const createMatrix = (row, column) => {
@@ -139,14 +217,5 @@ const createMatrix = (row, column) => {
   }
   console.log(matrix);
 };
-
-// const mouseOverHandler = (e) => {
-//   const item = e.target;
-//   item.style.border = "1px solid black";
-// };
-// const mouseOutHandler = (e) => {
-//   const item = e.target;
-//   item.style.border = "none";
-// };
 
 gameInit();
